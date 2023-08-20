1c5131f672193e9d190ae0cb22a37f506d0534d6
fk2019 <fk2019@gmail.com>
2023-08-20 19:56:07 +0300

    Add starwars

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+request(url, (err, response, body) => {
+  if (err) console.log(err);
+  const data = JSON.parse(body).characters;
+  const dataPromise = data.map(
+    (url) => new Promise((resolve, reject) => {
+      request(url, (promiseErr, __, dataBody) => {
+        if (promiseErr) reject(promiseErr);
+        resolve(JSON.parse(dataBody).name);
+      });
+    }),
+  );
+  Promise.all(dataPromise)
+    .then((name) => console.log(name.join('\n')))
+    .catch((err) => console.log(err));
+});
