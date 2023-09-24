3d7cbbb038bf7e10ba531a6087c7116e16dafb92
fk2019 <fk2019@gmail.com>
2023-09-24 20:21:23 +0300

    Add island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Island perimeter"""
+
+
+def island_perimeter(grid):
+    if not grid:
+        return 0
+    perimeter = 0
+    rows, cols = len(grid), len(grid[0])
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4  # Each land cell contributes 4 sides
+                # Check adjacent cells (up, down, left, right)
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2  # Deduct 2 sides for adjacent land
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+    return perimeter
