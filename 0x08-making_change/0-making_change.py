6083c94274c203a5a6b13cc55fbc458919e2846e
fk2019 <fk2019@gmail.com>
2023-11-23 16:32:35 +0300

    Refactor code and use dynamic programming

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,21 +5,12 @@ import sys
 
 
 def makeChange(coins, total):
-    """Return fewest number of coins needd to make toatl"""
-    def minCoins(total):
-        """Recursive call for sub problems"""
-        if total == 0:
-            return 0
-        if dp[total] != -1:
-            return dp[total]
-        res = sys.maxsize
-        for coin in coins:
-            if coin <= total:
-                sub_res = minCoins(total - coin)
-                if sub_res != sys.maxsize and sub_res + 1 < res:
-                    res = sub_res + 1
-        dp[total] = res
-        return res
-    dp = [-1] * (total + 1)
-    result = minCoins(total)
-    return result if result != sys.maxsize else -1
+    """Return fewest number of coins needd to make total"""
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
