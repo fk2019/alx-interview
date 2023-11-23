65bcfc27a76a6d5dcbd211dc12a736381538bd4e
fk2019 <fk2019@gmail.com>
2023-11-23 12:49:05 +0300

    Remove reduntant check

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,9 +4,6 @@
 
 
 def makeChange(coins, total):
-    if total <= 0:
-        return 0
-
     change_list = [float('inf')] * (total + 1)
     change_list[0] = 0
 
@@ -15,7 +12,4 @@ def makeChange(coins, total):
             change_list[amount] = min(change_list[amount],
                                       change_list[amount - coin] + 1)
 
-    if change_list[total] == float('inf'):
-        return -1
-    else:
-        return change_list[total]
+    return change_list[total] if change_list[total] != float('inf') else -1
